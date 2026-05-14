3226803ab51964bdb0634abf0c09d85e90c9bbcf
Amir <amirmero735@gmail.com>
2026-05-14 22:23:37 +0300

    add response signal to the output message

--- src/routes/data.py
+++ src/routes/data.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, APIRouter, Depends, UploadFile, File
+from fastapi import FastAPI, APIRouter, Depends, UploadFile, File, status
+from fastapi.responses import JSONResponse
 import os
 from helpers.config import get_settings, settings
 from controllers import DataController
@@ -12,6 +13,16 @@ async def upload_data(project_id : str,file: UploadFile=File(...),
                       app_settings: settings=Depends(get_settings)):
     
 
-    is_valid=DataController().validate_upladed_file(file=file)
+    is_valid, response_signal=DataController().validate_upladed_file(file=file)
 
-    return is_valid
+    #return {
+    #   "is_valid": is_valid,
+    #   "response_signal": response_signal.value
+    #}
+    if not is_valid:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "message" : response_signal.value
+            }
+        )
