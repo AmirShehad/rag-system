d282b1d40a23414d5f8baecfcd0ee182750509aa
Amir <amirmero735@gmail.com>
2026-05-22 23:22:30 +0300

    upload file with chunking method and make unique file path and add to file

--- src/routes/data.py
+++ src/routes/data.py
@@ -5,6 +5,11 @@ from helpers.config import get_settings, settings
 from controllers import DataController, ProjectController
 from models import ResponseSignal
 import aiofiles
+import logging
+
+logger=logging.getLogger('uvicorn.error')
+
+data_controller=DataController()
 data_router=APIRouter(
     prefix="/api/v1/data",
     tags=["api_v1","data"]
@@ -15,7 +20,7 @@ async def upload_data(project_id : str,file: UploadFile=File(...),
                       app_settings: settings=Depends(get_settings)):
     
 
-    is_valid, response_signal=DataController().validate_upladed_file(file=file)
+    is_valid, response_signal=data_controller.validate_uploaded_file(file=file)
     #if is_valid:
     #    return {
     #   "is_valid": is_valid,
@@ -28,16 +33,31 @@ async def upload_data(project_id : str,file: UploadFile=File(...),
                 "message" : response_signal.value
             }
         )
-    project_dir_path=ProjectController().project_path(project_id=project_id)
-    file_path=os.path.join(
-        project_dir_path,file.filename
+    project_dir_path=ProjectController().get_project_path(project_id=project_id)
+    
+    #file_path=os.path.join(
+    #    project_dir_path,file.filename)
+    
+    file_path, file_id=data_controller.generate_unique_filepath(
+        orign_filename=file.filename,
+        project_id=project_id
     )
-    async with aiofiles.open(file_path,"wb") as f:
-        while chunk:= await file.read(app_settings.FILE_CHUNK_SIZE):
-            await f.write(chunk)
+    try:   
+        async with aiofiles.open(file_path,"wb") as f:
+            while chunk:= await file.read(app_settings.FILE_CHUNK_SIZE):
+                await f.write(chunk)
+    except Exception as e:
 
+        logger.error(f"Error while uploading file:{e}")
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "message" : responseSignal.File_UPLOAD_FAILED.value
+            }
+        )
     return JSONResponse(
         content={
-            "signal" : ResponseSignal.FILE_UPLOAD_SUCCESS.value
+            "signal" : ResponseSignal.FILE_UPLOAD_SUCCESS.value,
+            "file_id" : file_id
         } 
     )        
