554c18f29e757363ecd4ec2ddfb5778f970dc61a
Amir <amirmero735@gmail.com>
2026-06-20 00:08:21 +0300

    add asset_id to DataChunk

--- src/models/db_schemes/DataChunk.py
+++ src/models/db_schemes/DataChunk.py
@@ -8,6 +8,7 @@ class DataChunk(BaseModel):
     chunk_metadata: dict
     chunk_order: int = Field(..., gt=0)
     chunk_project_id: str =ObjectId
+    chunk_asset_id: ObjectId
 
     class Config:
         arbitrary_types_allowed = True
